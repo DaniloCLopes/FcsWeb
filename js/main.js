59aca9dd093144b03cfdb5106e41fa61eb7a60fd
Danilo Lopes <danilocesarlopesfocus@outlook.com.br>
2024-05-21 12:31:03 -0300

    api adicionada

--- js/main.js
+++ js/main.js
@@ -43,7 +43,76 @@
         // });
 
     }; // end ssPreloader
-
+    const stopButton1 = document.querySelector('#stopButton1'),
+    stopButton2 = document.querySelector('#stopButton2'),
+    stopButton3 = document.querySelector('#stopButton3'),
+    stopButton4 = document.querySelector('#stopButton4'),
+    stopButton5 = document.querySelector('#stopButton5'),
+    stopButton6 = document.querySelector('#stopButton6'),
+    stopButton7 = document.querySelector('#stopButton7');
+    
+  const c4 = 261.6,
+      d4 = 293.7,
+    e4 = 329.6,
+    f4 = 349.2,
+    g4 = 392.0,
+    a4 = 440.0,
+    b4 = 493.9;
+  
+        
+  let context,
+      oscillator,
+    contextGain,
+    x = 1,
+    type = 'sine',
+    frequency;
+  
+  function start(){
+      context = new AudioContext();
+      oscillator = context.createOscillator();
+    contextGain = context.createGain();
+    oscillator.frequency.value = frequency;
+    oscillator.type = type;
+    oscillator.connect(contextGain);
+      contextGain.connect(context.destination);
+      oscillator.start(0);
+  }
+  
+  function stop(){
+    start();
+    contextGain.gain.exponentialRampToValueAtTime(
+        0.00001, context.currentTime + x
+      )
+  }
+  
+  stopButton1.addEventListener('click', function(){
+      frequency = c4;
+    stop();
+  });
+  stopButton2.addEventListener('click', function(){
+      frequency = d4;
+    stop();
+  });
+  stopButton3.addEventListener('click', function(){
+      frequency = e4;
+    stop();
+  });
+  stopButton4.addEventListener('click', function(){
+      frequency = f4;
+    stop();
+  });
+  stopButton5.addEventListener('click', function(){
+      frequency = g4;
+    stop();
+  });
+  stopButton6.addEventListener('click', function(){
+      frequency = a4;
+    stop();
+  });
+  stopButton7.addEventListener('click', function(){
+      frequency = b4;
+    stop();
+  });
 
    /* move header
     * -------------------------------------------------- */
